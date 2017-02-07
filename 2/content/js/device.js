60ca0219377f8b746f961eafc1fdbd9548ff51ce
Olexandr Panov <futupas@gmail.com>
2017-02-07 18:41:20 +0200

    2 version

---
+++ 2/content/js/device.js
@@ -0,0 +1,17 @@
+// Device.js
+// (c) 2014 Matthew Hudson
+
+var Device = {
+    find = function (needle) {
+        return window.navigator.userAgent.toLowerCase().indexOf(needle) !== -1;
+    },
+    IsMobile = function () {
+        return (!Device.find('windows') && Device.find('android') && Device.find('mobile')) || 
+            (!Device.find('windows') && Device.find('iphone')) || 
+            (Device.find('ipod')) || 
+            (Device.find('windows') && Device.find('phone')) || 
+            ((Device.find('blackberry') || Device.find('bb10') || Device.find('rim')) && !Device.find('tablet')) || 
+            (((Device.find('(mobile;') || Device.find('(tablet;')) && Device.find('; rv:')) && Device.find('mobile')) || 
+            (Device.find('meego'));
+    }
+}
